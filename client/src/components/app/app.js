5bdc89f673172168d67f5adfd3fb97a3880f0a71
jekkyWa <72879793+jekkyWa@users.noreply.github.com>
2021-02-23 01:17:43 +0300

    update

---
+++ client/src/components/app/app.js
@@ -0,0 +1,8 @@
+import Authentication from "../authentication";
+import "./app.css";
+
+const App = () => {
+  return <Authentication />;
+};
+
+export default App;
---
+++ client/src/components/authentication/authentication.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./authentication.scss";
+
+
+const Authentication = () => {
+  return (
+    <div className="authentication">
+      <h1>nieTask</h1>
+      <div className="authentcation-form">
+        <h2>Register an account</h2>
+        <div className="mail">
+          <input placeholder="Please enter your email" />
+        </div>
+        <div className="name">
+          <input placeholder="Please enter full name" />
+        </div>
+        <div className="password">
+          <input placeholder="Please create a password" />
+        </div>
+        <div className="btn-register">
+        <button>Register</button>
+        </div>
+        <hr/>
+        <p>Already have an account, <span className="sign-in">sign in </span>?</p>
+      </div>
+    </div>
+  );
+};
+
+export default Authentication;
