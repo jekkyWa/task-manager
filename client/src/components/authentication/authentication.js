e03b7d2e8543427b4c56c7bb598a470fa4d28e8e
jekkyWa <72879793+jekkyWa@users.noreply.github.com>
2021-02-23 16:27:16 +0300

    fixed page of login and add password-script

--- client/src/components/authentication/authentication.js
+++ client/src/components/authentication/authentication.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./authentication.scss";
+import VisibilityIcon from "@material-ui/icons/Visibility";
+import VisibilityOutlinedIcon from "@material-ui/icons/VisibilityOutlined";
 
 const Authentication = () => {
+  const [statePassword, setStatePassword] = useState(true);
+  const [form, setForm] = useState({ email: "", password: "", name: "" });
+  const [warningMessage, setWarningMessage] = useState("");
+
+  const changeHandler = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const displayPassword = () => {
+    let value = statePassword ? false : true;
+    setStatePassword(value);
+  };
+
+  const reliabilityCheck = () => {
+    const memo = () => {
+      if (_err !== warningMessage) setWarningMessage(_err);
+    };
+    let _err = "";
+    if (form.password.length < 9 && form.password.length !== 0) {
+      _err = "Пароль должен быть не менее 8 символов";
+      memo();
+    } else {
+      _err = "Низкий";
+      memo();
+    }
+  };
+
+  useEffect(() => {
+    reliabilityCheck();
+  }, [form.password]);
+
   return (
     <div className="authentication">
       <h1>nieTask</h1>
@@ -14,9 +47,32 @@ const Authentication = () => {
           <input placeholder="Please enter full name" />
         </div>
         <div className="password">
-          <input placeholder="Please create a password" />
+          <input
+            type={statePassword ? "password" : "text"}
+            placeholder="Please create a password"
+            name="password"
+            onChange={changeHandler}
+          />
+          <div className="eye-icon-block">
+            <VisibilityIcon
+              onClick={displayPassword}
+              className={statePassword ? "eye-icon-hidden" : "eye-icon"}
+            />
+            <VisibilityOutlinedIcon
+              onClick={displayPassword}
+              className={
+                statePassword ? "eye-icon eye-icon-vis" : "eye-icon-hidden"
+              }
+            />
+          </div>
         </div>
-       
+        <div className="d">
+          <div className="a">
+            <span className="b"></span>
+          </div>
+          <p className="c"></p>
+        </div>
+        <p>{warningMessage}</p>
         <div className="btn-register">
           <button>Register</button>
         </div>
