73ed1f001a9abedcbfc44042ad91dcc2f897702a
jekkyWa <72879793+jekkyWa@users.noreply.github.com>
2021-02-27 23:22:11 +0300

    update Models

--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ const shema = new Schema({
     email: {type: String, required: true, unique: true},
     name: {type: String, required: true},
     password: {type: String, required: true},
-    id: {type: Number},
+    active_rooms:[],
+    passive_rooms:[]
 });
 
 module.exports = model("User", shema);
