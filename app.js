c76666962ccecdf2fe427fc4c12eee269e1c1ec3
jekkyWa <72879793+jekkyWa@users.noreply.github.com>
2021-03-01 17:59:00 +0300

    update app-rout

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
-const express = require('express');
+const express = require("express");
 const config = require("config");
 const mongoose = require("mongoose");
-const cors = require("cors")
+const cors = require("cors");
 
 const app = express();
 
-app.use(express.json({extended: true}));
+app.use(express.json({ extended: true }));
 
 app.use(
-    cors({
-        credentials: true,
-        origin: ["http://localhost:3000"],
-        optionsSuccessStatus: 200,
-    }))
+  cors({
+    credentials: true,
+    origin: ["http://localhost:3000"],
+    optionsSuccessStatus: 200,
+  })
+);
 
-app.use('/api/auth', require('./routes/auth.routes'))
+app.use("/api/auth", require("./routes/auth.routes"));
+
+app.use("/api/getData", require('./routes/data.routes'))
 
 const PORT = config.get("port") || 5000;
 
 async function start() {
-    try {
-        await mongoose.connect(config.get("mongoUri"), {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-        });
-        app.listen(PORT, () => console.log(`App has been started on ${PORT}`));
-    } catch (e) {
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(config.get("mongoUri"), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    app.listen(PORT, () => console.log(`App has been started on ${PORT}`));
+  } catch (e) {
+    process.exit(1);
+  }
 }
 
 start();
