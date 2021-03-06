8fbfd98a3fad8a8758e5395bd7bb2d7187385491
jekkyWa <72879793+jekkyWa@users.noreply.github.com>
2021-03-06 03:05:34 +0300

    Update Board models

--- models/Board.js
+++ models/Board.js
@@ -1,9 +1,12 @@
 const { Schema, model, Types } = require("mongoose");
 
 const shema = new Schema({
-  board_id: { type: Number },
-  descriptions: [],
+  board_id: { type: String },
+  name_Project: { type: String },
+  description: { type: String },
+  date: { type: String },
+  addedUsers: [],
   task: [],
 });
 
-module.exports = model("User", shema);
+module.exports = model("Board", shema);
