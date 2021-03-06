6389bbca9c0f24777af8fe6d9d2d05cb4cffc242
jekkyWa <72879793+jekkyWa@users.noreply.github.com>
2021-03-06 03:05:13 +0300

    update routes

--- routes/board.routes.js
+++ routes/board.routes.js
@@ -2,23 +2,34 @@ const { Router } = require("express");
 const router = Router();
 const auth = require("../middleware/auth.middleware");
 const Board = require("../models/Board");
+const User = require("../models/User");
 const ObjectId = require("mongodb").ObjectId;
 const shortid = require("shortid");
 
-router.post("/test", auth, async (req, res) => {
+router.post("/createBoard", auth, async (req, res) => {
   try {
     const { nameProject, description, date, addedUsers } = req.body;
 
-    let id = shortid.generate()
+    let id = shortid.generate();
 
     const board = new Board({
-      nameProject,
+      name_Project: nameProject,
       description,
       date,
       addedUsers,
-      id
+      board_id: id,
     });
 
+    await board.save();
+
+    const value = await User.find({ _id: ObjectId(req.user.userId) });
+
+    await User.updateOne(value[0], {
+      ...value,
+      active_rooms: [id],
+    });
+
+    res.status(201).json({ message: "Доска создана" });
   } catch (e) {
     res.status(500).json({ message: "Что-то пошло не так, попробуйте снова" });
   }
--- routes/data.routes.js
+++ routes/data.routes.js
@@ -7,7 +7,11 @@ const ObjectId = require("mongodb").ObjectId;
 router.get("/test", auth, async (req, res) => {
   try {
     const value = await User.find({ _id: ObjectId(req.user.userId) });
-    res.json({ email: value[0].email, name: value[0].name });
+    res.json({
+      email: value[0].email,
+      name: value[0].name,
+      active_rooms: value[0].active_rooms,
+    });
   } catch (e) {
     res.status(500).json({ message: "Что-то пошло не так, попробуйте снова" });
   }
