0e118456fcbfc4b12c19df6ca30400e4e4e57850
jekkyWa <72879793+jekkyWa@users.noreply.github.com>
2021-03-23 21:47:37 +0300

    add new srting

--- models/Cards.js
+++ models/Cards.js
@@ -3,8 +3,9 @@ const { Schema, model, Types } = require("mongoose");
 const shema = new Schema({
   card_id: { type: String },
   name_Board: { type: String },
-  cards: {type:Array},
+  cards: { type: Array },
   color: { type: String },
+  recentActivity: [],
 });
 
 module.exports = model("Cards", shema);
