0355efd6a38073ae1d660410fc660f25de5d1e01
jekkyWa <72879793+jekkyWa@users.noreply.github.com>
2021-04-06 19:16:13 +0300

    add level up and down

--- app.js
+++ app.js
@@ -132,8 +132,28 @@ io.on("connection", (socket) => {
     board[0].addedUsers[indexBoard].role = data;
     await Board.updateOne(boardOriginal[0], board[0]);
 
-    // io.emit("getUpdateRoleInCommand", { board: board[0], user: user[0] });
+    io.emit("getUpdateRoleInCommand", { board: board[0] });
   });
+
+  socket.on(
+    "updateLevelInCommand",
+    async ({ id, email, movement, currentState }) => {
+      const board = await Board.find({ board_id: id });
+      let boardOriginal = JSON.parse(JSON.stringify(board));
+      const indexBoard = board[0].addedUsers.findIndex((e) => e.email == email);
+      const levelArr = ["Junior", "Middle", "Senior"];
+      if (movement == "up" && currentState !== 2) {
+        board[0].addedUsers[indexBoard].level = levelArr[currentState + 1];
+      }
+      if (movement == "down" && currentState !== 0) {
+        board[0].addedUsers[indexBoard].level = levelArr[currentState - 1];
+      }
+      if (JSON.stringify(board) !== JSON.stringify(boardOriginal)) {
+        await Board.updateOne(boardOriginal[0], board[0]);
+        io.emit("getUpdateLevelInCommand", { board: board[0] });
+      }
+    }
+  );
 
   // --------- Cards
 
